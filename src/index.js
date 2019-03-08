4eab6c192b816486d614d4dd08bf3fd6b4b79932
dreamer <wildetangeant@gmail.com>
2019-03-07 18:07:55 -0800

    search -> URL binding established

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { loadCards } from '../src/card-constructor.js';
+import '../src/browser-to-search.js';
 
 const APIURL = 'https://rickandmortyapi.com/api/character/';
 
 fetch(APIURL).then(response => response.json()).then(body => loadCards(body.results));
+
+//get a hash change listener di
+
+
+window.addEventListener('hashChange', loadQuery);
